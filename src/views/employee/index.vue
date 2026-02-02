9fd83cf65b0dc1cf1e0e9561dc28c3f00bc208d8
arthuryin1314 <1736424125@qq.com>
2026-02-02 18:13:46 +0800

    完成day6

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -2,8 +2,25 @@
   <div class="container">
     <div class="app-container">
       <div class="left">
-        <el-input style="margin-bottom:10px" type="text" prefix-icon="el-icon-search" size="small" placeholder="输入员工姓名全员搜索" />
-        <!-- 树形组件 -->
+        <el-input
+          v-model="params.keyword"
+          style="margin-bottom:10px"
+          type="text"
+          prefix-icon="el-icon-search"
+          size="small"
+          placeholder="输入员工姓名全员搜索"
+          @input="search"
+        />
+        <el-tree
+          ref="tree"
+          :data="data"
+          :props="defaultProps"
+          default-expand-all
+          highlight-current
+          :expand-on-click-node="false"
+          node-key="id"
+          @current-change="selectNode"
+        />
       </div>
       <div class="right">
         <el-row class="opeate-tools" type="flex" justify="end">
@@ -11,16 +28,114 @@
           <el-button size="mini">excel导入</el-button>
           <el-button size="mini">excel导出</el-button>
         </el-row>
-        <!-- 表格组件 -->
-        <!-- 分页 -->
+        <el-table :data="tableData" style="width: 100%" :default-sort="{ prop: 'timeOfEntry', order: 'descending' }">
+          <el-table-column prop="staffPhoto" label="头像" width="100">
+            <template slot-scope="{ row }">
+              <img :src="row.staffPhoto" alt="" style="width: 32px; height: 32px; border-radius: 50%;">
+            </template>
+          </el-table-column>
+          <el-table-column prop="username" label="姓名" width="180" />
+          <el-table-column prop="mobile" label="手机" :formatter="formatter" sortable />
+          <el-table-column prop="workNumber" label="工号" sortable />
+          <el-table-column prop="formOfEmployment" label="聘用形式">
+            <template slot-scope="{ row }">
+              <span>{{ row.formOfEmployment==1?'正式':'非正式' }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="departmentName" label="部门" />
+          <el-table-column prop="timeOfEntry" label="入职时间" sortable />
+          <el-table-column label="操作">
+            <template>
+              <el-button type="text" size="small">
+                查看
+              </el-button>
+              <el-button type="text" size="small">
+                角色
+              </el-button>
+              <el-button type="text" size="small">
+                删除
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination
+          class="pagination"
+          background
+          layout="prev, pager, next"
+          :total="total"
+          @current-change="changePage($event)"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { debounce } from 'lodash'
+import { getDepartmentList } from '@/api/department'
+import { getEmployeeList } from '@/api/employee'
+import { transListToTreeData } from '@/utils/index'
 export default {
-  name: 'Employee'
+  name: 'Employee',
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        label: 'name',
+        children: 'children'
+      },
+      total: 0,
+      params: {
+        departmentId: '',
+        page: 1,
+        pagesize: 10,
+        keyword: ''
+      },
+      tableData: [
+      ]
+    }
+  },
+  created() {
+    this.getDepartmentList()
+    this.debouncedSearch = debounce(() => {
+      this.params.page = 1
+      this.getEmployeeList()
+    }, 500)
+  },
+  methods: {
+    async getDepartmentList() {
+      this.data = transListToTreeData(await getDepartmentList(), 0)
+      // console.log(this.data[0])
+      this.params.departmentId = this.data[0].id
+      // console.log(this.params.departmentId)
+      this.$nextTick(() => {
+        this.$refs.tree.setCurrentKey(this.params.departmentId)
+      })
+      this.getEmployeeList()
+    },
+    selectNode(node) {
+      // console.log(node)
+      this.params.departmentId = node.id
+      this.getEmployeeList()
+    },
+    formatter(row, column, cellValue) {
+      return cellValue
+    },
+    async getEmployeeList() {
+      const { rows, total } = await getEmployeeList(this.params)
+      this.tableData = rows
+      this.total = total
+    },
+    changePage(page) {
+      // console.log(page)
+      this.params.page = page
+      this.getEmployeeList()
+    },
+    search() {
+      // console.log(this.params.keyword)
+      this.debouncedSearch()
+    }
+  }
 }
 </script>
 
@@ -28,17 +143,27 @@ export default {
 .app-container {
   background: #fff;
   display: flex;
+
   .left {
     width: 280px;
     padding: 20px;
     border-right: 1px solid #eaeef4;
   }
+
   .right {
     flex: 1;
     padding: 20px;
+
     .opeate-tools {
-      margin:10px ;
+      margin: 10px;
     }
+
+    .pagination {
+      margin-top: 10px;
+      display: flex;
+      justify-content: flex-end;
+    }
+
     .username {
       height: 30px;
       width: 30px;
@@ -48,9 +173,8 @@ export default {
       color: #fff;
       background: #04C9BE;
       font-size: 12px;
-      display:inline-block;
+      display: inline-block;
     }
   }
 }
-
 </style>
